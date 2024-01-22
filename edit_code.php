fa53005b3ae117c9e07d88f319294ea3615006fe
ngatngay <ngatngay@ngatngay.net>
2024-01-22 13:32:48 +0700

    update to 1.0.10

--- edit_code.php
+++ edit_code.php
@@ -37,7 +37,7 @@ if ($dir == null || $name == null || !is_file(processDirectory($dir . '/' . $nam
     $codeType = [
         'text' => 'Text',
         'php' => 'PHP',
-        'js' => 'JavaScript',
+        'javascript' => 'JavaScript',
     ];
 
     if (array_key_exists($fileExt, $codeType)) {
@@ -66,42 +66,70 @@ if ($dir == null || $name == null || !is_file(processDirectory($dir . '/' . $nam
             <hr/>
         </div>
 
-        <style type="text/css" media="screen">
-            #editor {
-                width: 100%;
-                height: 500px;
-                font-size: 14px;
-            }
+        <form id="code_form" action="javascript:void(0)">
+            <div>
+                <textarea id="editor" class="box_edit">' . htmlspecialchars($content) . '</textarea>
+            </div>
+            <div class="input_action">
+                <input type="submit" value="Lưu lại" />
+            </div>
+        </form>
 
-            .ace_scroller, .ace_gutter {
-                padding-top: 10px;
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/codemirror.min.css" integrity="sha512-uf06llspW44/LZpHzHT6qBOIVODjWtv4MxCricRxkzvopAlSWnTf6hpZTFxuuZcuNE9CBQhqE0Seu1CoRk84nQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/codemirror.min.js" integrity="sha512-OeZ4Yrb/W7d2W4rAMOO0HQ9Ro/aWLtpW9BUSR2UOWnSV2hprXLkkYnnCGc9NeLUxxE4ZG7zN16UuT1Elqq8Opg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/theme/darcula.min.css" integrity="sha512-kqCOYFDdyQF4JM8RddA6rMBi9oaLdR0aEACdB95Xl1EgaBhaXMIe8T4uxmPitfq4qRmHqo+nBU2d1l+M4zUx1g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/addon/selection/active-line.min.js" integrity="sha512-0sDhEPgX5DsfNcL5ty4kP6tR8H2vPkn40GwA0RYTshkbksURAlsRVnG4ECPPBQh7ZYU6S3rGvp5uhlGQUNrcmA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/mode/htmlmixed/htmlmixed.min.js" integrity="sha512-HN6cn6mIWeFJFwRN9yetDAMSh+AK9myHF1X9GlSlKmThaat65342Yw8wL7ITuaJnPioG0SYG09gy0qd5+s777w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/mode/xml/xml.min.js" integrity="sha512-LarNmzVokUmcA7aUDtqZ6oTS+YXmUKzpGdm8DxC46A6AHu+PQiYCUlwEGWidjVYMo/QXZMFMIadZtrkfApYp/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/mode/css/css.min.js" integrity="sha512-rQImvJlBa8MV1Tl1SXR5zD2bWfmgCEIzTieFegGg89AAt7j/NBEe50M5CqYQJnRwtkjKMmuYgHBqtD1Ubbk5ww==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/mode/clike/clike.min.js" integrity="sha512-jcF10R6LSoLddMx32eEitiBfJ8icHBobh0Z7fwVewrKmNBBGM0B09oG3yxxnkIYwilUsXBbIcRN5jfmc6vSt9g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/mode/javascript/javascript.min.js" integrity="sha512-Cbz+kvn+l5pi5HfXsEB/FYgZVKjGIhOgYNBwj4W2IHP2y8r3AdyDCQRnEUqIQ+6aJjygKPTyaNT2eIihaykJlw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.65.16/mode/php/php.min.js" integrity="sha512-jZGz5n9AVTuQGhKTL0QzOm6bxxIQjaSbins+vD3OIdI7mtnmYE6h/L+UBGIp/SssLggbkxRzp9XkQNA4AyjFBw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <style type="text/css" media="screen">
+            @media (min-width: 240px) {
+                .CodeMirror {
+                    height: 320px;
+                }
+            }
+            
+            @media (min-width: 320px) {
+                .CodeMirror {
+                    height: 480px;
+                }
+            }
+            
+            @media (min-width: 480px) {
+                .CodeMirror {
+                    height: 640px;
+                }
+            }
+            
+            @media (min-width: 640px) {
+                .CodeMirror {
+                    height: 720px;
+                }
             }
         </style>
-
-        <div id="editor" contenteditable="true">' . htmlspecialchars($content) . '</div>
-
-        <div class="input_action">
-            <form id="code_form" action="javascript:void(0)">
-                <input type="submit" value="Lưu lại" />
-            </form>
-        </div>
-
-        <script src="https://cdn.jsdelivr.net/npm/ace-builds@1.32.2/src-min-noconflict/ace.min.js"></script>
-        <link href="https://cdn.jsdelivr.net/npm/ace-builds@1.32.2/css/ace.min.css" rel="stylesheet">
         
         <script>
             var codeLang = "' . $codeLang . '";
-            var editor = ace.edit("editor", {
-                useWorker: false,
-                showPrintMargin: false,
-                mode: "ace/mode/" + codeLang,
-                theme: "ace/theme/one_dark"
+            const editorElement = document.getElementById("editor");
+            var editor = CodeMirror.fromTextArea(editorElement, {
+                mode: codeLang,
+                theme: "darcula",
+                lineNumbers: true,
+                fixedGutter: false,
+                styleActiveLine: true,
             });
             
             var codeLangElement = document.getElementById("code_lang");
             codeLangElement.addEventListener("change", function () {
                 var mode = codeLangElement.value;
-                editor.session.setMode("ace/mode/" + mode);
+                editor.setOption("mode", mode);
             });
             
             var codeFormElement = document.getElementById("code_form");
