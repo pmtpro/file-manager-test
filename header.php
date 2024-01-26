fc5707aacf43fe7a34754efabd2ab70012b550db
ngatngay <ngatngay@ngatngay.net>
2024-01-26 22:07:20 +0700

    fix css link

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <title><?php echo $title; ?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="style.css" media="all,handheld" />
+    <link rel="stylesheet" type="text/css" href="<?php asset('style.css') ?>" media="all,handheld" />
     <link rel="icon" type="image/png" href="icon/icon.png">
     <link rel="icon" type="image/x-icon" href="icon/icon.ico" />
     <link rel="shortcut icon" type="image/x-icon" href="icon/icon.ico" />
