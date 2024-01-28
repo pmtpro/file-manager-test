b1bd3695519dc20953832ffc7c72661f2a0b0cb8
linh0804 <77475414+linh0804@users.noreply.github.com>
2024-01-28 13:41:13 +0700

    Update format_code.php
    fix format

--- format_code.php
+++ format_code.php
@@ -1,16 +1,18 @@
 <?php
-    $dirFixer = __DIR__ . '/lib/format';
-    $tempFile = $dirFixer .'/fixer.txt';   
+    $tempFile = __DIR__ .'/fixer.txt'; 
+    $cacheFile = __DIR__ .'/.php-cs-fixer.cache';  
     $data = array(
       'highlight' => '',
-      'error' => 'Không thành công! Chỉnh dành cho code php'
+      'error' => 'Không thành công! yêu cầu cài php-cs-fixer vào manager'
     );
     $content = isset($_POST['content']) ? $_POST['content'] : null;
     if(isset($content) && isset($_POST['requestApi'])){       
         file_put_contents($tempFile, $content);
-        if(exec('cd '. $dirFixer .' && vendor/bin/php-cs-fixer fix fixer.txt')){
-          $data['highlight'] = file_get_contents($tempFile);
-          $data['error'] = '';
+        if(exec('cd '. __DIR__ .' && vendor/bin/php-cs-fixer fix fixer.txt')){
+            $data['highlight'] = file_get_contents($tempFile);
+            $data['error'] = '';
+            @unlink($tempFile);
+            @unlink($cacheFile);
         } 
     }
     header('Content-Type: application/json; charset=utf-8');
